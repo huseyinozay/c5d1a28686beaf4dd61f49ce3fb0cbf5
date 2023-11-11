96fc1b7735a8433f861e59553ed4405b5f9ec88e
huseyinozay <huseyinozay27@gmail.com>
2023-11-12 02:42:30 +0300

    added vue-router and primevue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
 import "./style.css";
 import App from "./App.vue";
 
-createApp(App.use(createPinia())).mount("#app");
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+
+createApp(App).use(createPinia).use(PrimeVue).mount("#app");
