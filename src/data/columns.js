b5bcc5734f2a0f2acd965a9744b0678ec44e8060
huseyinozay <huseyinozay27@gmail.com>
2023-11-12 21:46:29 +0300

    feature: created column js and added columns

---
+++ src/data/columns.js
@@ -0,0 +1,45 @@
+export const currencyTimeSeriesColumns = [
+  {
+    field: "date",
+    header: "Date",
+  },
+  {
+    field: "fromCurrency",
+    header: "From Currency",
+  },
+  {
+    field: "toCurrency",
+    header: "To Currency",
+  },
+  {
+    field: "rate",
+    header: "Rate",
+  },
+];
+
+export const currencyConversionHistoryColumns = [
+  {
+    field: "timestamp",
+    header: "Timestamp",
+  },
+  {
+    field: "date",
+    header: "Date",
+  },
+  {
+    field: "fromCurrency",
+    header: "From Currency",
+  },
+  {
+    field: "toCurrency",
+    header: "To Currency",
+  },
+  {
+    field: "amount",
+    header: "Amount",
+  },
+  {
+    field: "rate",
+    header: "Rate",
+  },
+];
