ab89656e9e761c5ab6a00d8cc0cdaca1974b84d4
huseyinozay <huseyinozay27@gmail.com>
2023-11-12 13:11:35 +0300

    setup: created pages and router

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    name: "CurrencyConversion",
+    component: () => import("../views/currencyConversion/Index.vue"),
+  },
+  {
+    path: "/currencyTimeSeries",
+    name: "CurrencyTimeSeries",
+    component: () => import("../views/currencyTimeSeries/Index.vue"),
+  },
+  {
+    path: "/currencyConversationHistory",
+    name: "CurrencyConversationHistory",
+    component: () => import("../views/currencyConversationHistory/Index.vue"),
+  },
+];
