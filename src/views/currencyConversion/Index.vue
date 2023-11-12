30c24fa978a933290358c14b73ba04e7191329c7
huseyinozay <huseyinozay27@gmail.com>
2023-11-12 21:48:59 +0300

    feature: currency-conversion page's elements have been placed

--- src/views/currencyConversion/Index.vue
+++ src/views/currencyConversion/Index.vue
@@ -1,3 +1,35 @@
 <template>
   <p>Currency conversion</p>
+
+  <div class="grid">
+    <div class="col">
+      <Calendar v-model="date" showIcon />
+    </div>
+  </div>
+  <div class="grid">
+    <div class="col-3 col-offset-3">
+      <InputText />
+    </div>
+    <div class="col-3">
+      <Dropdown class="w-full md:w-14rem" />
+    </div>
+  </div>
+  <div class="grid">
+    <div class="col-3 col-offset-3">
+      <InputText />
+    </div>
+    <div class="col-3">
+      <Dropdown class="w-full md:w-14rem" />
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+import InputText from "primevue/inputtext";
+import Dropdown from "primevue/dropdown";
+import Calendar from "primevue/calendar";
+
+const date = ref();
+</script>
