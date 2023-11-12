4c4717530467a49351beedc50eff8bc129c91f9d
huseyinozay <huseyinozay27@gmail.com>
2023-11-12 21:49:39 +0300

    feature: currency-conversation-history page's elements have been placed

--- src/views/currencyConversationHistory/Index.vue
+++ src/views/currencyConversationHistory/Index.vue
@@ -1,3 +1,10 @@
 <template>
-  <p>Currency conversation history</p>
+  <div class="card">
+    <CustomDataTable :data="data" :columns="currencyConversionHistoryColumns" />
+  </div>
 </template>
+
+<script setup>
+import CustomDataTable from "../../components/CustomDataTable.vue";
+import { currencyConversionHistoryColumns } from "../../data/columns";
+</script>
