45d3f53cdf647320b622d4c317a70c8ccbd754a5
huseyinozay <huseyinozay27@gmail.com>
2023-11-12 21:45:01 +0300

    featuure: created custom datatable based on primevue datatable

---
+++ src/components/CustomDataTable.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card">
+    <DataTable :value="data" tableStyle="min-width: 50rem">
+      <Column
+        v-for="col of columns"
+        :key="col.field"
+        :field="col.field"
+        :header="col.header"
+      ></Column>
+    </DataTable>
+  </div>
+</template>
+
+<script setup>
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+
+const props = defineProps({
+  data: {
+    type: Array,
+    required: true,
+  },
+  columns: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
