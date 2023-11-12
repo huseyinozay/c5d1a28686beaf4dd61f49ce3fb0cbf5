d270e110488ce6a439074fb8697c4ac2f21a454f
huseyinozay <huseyinozay27@gmail.com>
2023-11-12 21:49:18 +0300

    feature: currency-time-series page's elements have been placed

--- src/views/currencyTimeSeries/Index.vue
+++ src/views/currencyTimeSeries/Index.vue
@@ -1,3 +1,43 @@
 <template>
-  <p>Currency time series</p>
+  <p>Currency conversion</p>
+
+  <div class="grid">
+    <div class="col">
+      <Calendar v-model="startDate" showIcon />
+    </div>
+    <div class="col">
+      <Calendar v-model="endDate" showIcon />
+    </div>
+    <div class="col">
+      <Dropdown class="w-full" />
+    </div>
+    <div class="col">
+      <Dropdown class="w-full" />
+    </div>
+    <div class="col">
+      <Button @click="() => console.log(startDate)" label="List" />
+    </div>
+  </div>
+  <div class="grid">
+    <div class="col-6">
+      <CustomDataTable :data="data" :columns="currencyTimeSeriesColumns" />
+    </div>
+    <div class="col-6">
+      <CustomChart />
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+import Dropdown from "primevue/dropdown";
+import Calendar from "primevue/calendar";
+import Button from "primevue/button";
+import CustomDataTable from "../../components/CustomDataTable.vue";
+import CustomChart from "../../components/CustomChart.vue";
+import { currencyTimeSeriesColumns } from "../../data/columns";
+
+const startDate = ref();
+const endDate = ref();
+</script>
