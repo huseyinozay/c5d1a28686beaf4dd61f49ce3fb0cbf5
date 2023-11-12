0f49a5629d4cc73d642d40bb61f0446cf4986ab3
huseyinozay <huseyinozay27@gmail.com>
2023-11-12 21:45:37 +0300

    feature: created custom chart based on primevue

---
+++ src/components/CustomChart.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="card">
+    <Chart
+      type="line"
+      :data="chartData"
+      :options="chartOptions"
+      class="h-30rem"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Chart from "primevue/chart";
+
+const documentStyle = getComputedStyle(document.documentElement);
+const textColor = documentStyle.getPropertyValue("--text-color");
+const textColorSecondary = documentStyle.getPropertyValue(
+  "--text-color-secondary"
+);
+const surfaceBorder = documentStyle.getPropertyValue("--surface-border");
+const chartData = ref({
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [
+    {
+      label: "First Dataset",
+      data: [65, 59, 80, 81, 56, 55, 40],
+      fill: false,
+      borderColor: documentStyle.getPropertyValue("--blue-500"),
+      tension: 0.4,
+    },
+    {
+      label: "Second Dataset",
+      data: [28, 48, 40, 19, 86, 27, 90],
+      fill: false,
+      borderColor: documentStyle.getPropertyValue("--pink-500"),
+      tension: 0.4,
+    },
+  ],
+});
+const chartOptions = ref({
+  maintainAspectRatio: false,
+  aspectRatio: 0.6,
+  plugins: {
+    legend: {
+      labels: {
+        color: textColor,
+      },
+    },
+  },
+  scales: {
+    x: {
+      ticks: {
+        color: textColorSecondary,
+      },
+      grid: {
+        color: surfaceBorder,
+      },
+    },
+    y: {
+      ticks: {
+        color: textColorSecondary,
+      },
+      grid: {
+        color: surfaceBorder,
+      },
+    },
+  },
+});
+</script>
