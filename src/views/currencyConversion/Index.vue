31924b1392441c7421a3ac007cedb75b3fe9e295
huseyinozay <huseyinozay27@gmail.com>
2023-11-14 06:36:17 +0300

    fix: watch fixed

--- src/views/currencyConversion/Index.vue
+++ src/views/currencyConversion/Index.vue
@@ -81,7 +81,7 @@ const convertUrl = computed(() => {
   }&amount=${fromAmount.value}&date=${outputDateFormatted(date.value)}`;
 });
 
-watch(async (convertUrl) => {
+watch(convertUrl, async (url) => {
   if (fromAmount.value !== "") {
     const { data } = await http.get(url);
     toAmount.value = data.result;
