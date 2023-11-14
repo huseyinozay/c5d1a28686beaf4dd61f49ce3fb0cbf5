de4314dd01a5c54cc96941de58f881af5e15ae68
huseyinozay <huseyinozay27@gmail.com>
2023-11-15 02:52:54 +0300

    feature: pagination added

--- src/components/CustomDataTable.vue
+++ src/components/CustomDataTable.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="card">
-    <DataTable :value="data" tableStyle="min-width: 50rem">
+    <DataTable
+      :value="data"
+      tableStyle="min-width: 50rem"
+      paginator
+      :rows="10"
+      :rowsPerPageOptions="[10, 20, 50]"
+    >
       <Column
         v-for="col of columns"
         :key="col.field"
